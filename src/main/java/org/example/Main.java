5df6f8047b36270baeb65678fd059e74213a586d
nunoteixeira <nunoteixeira@gmail.com>
2024-03-25 22:45:02 +0000

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Page page;
+        BrowserContext context;
+        BrowserType browserType;
+        BrowserType.LaunchOptions launchOptions;
+
+        try (Playwright playwright = Playwright.create()) {
+
+            browserType = playwright.chromium();
+            launchOptions = new BrowserType.LaunchOptions().setHeadless(false).setArgs(List.of("--start-maximized"));
+        }
+
+        try (Browser browser = browserType.launch(launchOptions)) {
+            context = browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
+        }
+
+        page = context.newPage();
+        page.navigate("https://www.saucedemo.com/");
+
+        List<String> users = new ArrayList<>();
+        List<Locator> locators = new ArrayList<>();
+    }
+}
