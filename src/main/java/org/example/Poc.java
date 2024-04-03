7aa690162d32a67385af9a449f05ea8963f43e38
nunoteixeira <nunoteixeira@gmail.com>
2024-04-03 13:56:54 +0100

    Add files via upload

--- src/main/java/org/example/Poc.java
+++ src/main/java/org/example/Poc.java
@@ -1,9 +1,12 @@
 package org.example;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class Poc {
     public static void main(String[] args) {
@@ -22,8 +25,24 @@ public class Poc {
                 page = context.newPage();
                 page.navigate("https://www.saucedemo.com/");
 
-                List<String> users = new ArrayList<>();
-                List<Locator> locators = new ArrayList<>();
+                List<String> users;
+                Locator credentialLocator = page.locator("#login_credentials");
+                Locator passwordLocator = page.locator(".login_password");
+                users = Arrays.stream(credentialLocator.allInnerTexts().get(0).split("\n")).filter(word -> word.contains("_user")).toList();
+                String user = users.stream().findAny().orElse(null);
+                String password = passwordLocator.textContent().split(":")[1];
+
+                Locator usernameInput = page.getByPlaceholder("Username");
+                Locator passwordInput = page.getByPlaceholder("Password");
+
+                usernameInput.fill(user);
+                passwordInput.fill(password);
+
+                assertThat(usernameInput).hasValue(user);
+                assertThat(passwordInput).hasValue(password);
+
+                Locator loginButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login"));
+                loginButton.click();
             }
         }
     }
